7086076803d4b7873b031057aab0de163903a679
Dagim <dagimgetaW27@gmail.com>
2024-05-01 10:51:43 +0300

    Button Add and Cancle function

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialFriends = [
   {
     id: 118836,
@@ -18,14 +20,28 @@ const initialFriends = [
     balance: 0,
   },
 ];
-
+function Button({ children, onClick }) {
+  return (
+    <button className="button" onClick={onClick}>
+      {children}
+    </button>
+  );
+}
 export default function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+
+  function handlShowAddFriend() {
+    setShowAddFriend((show) => !show);
+  }
+
   return (
     <div className="app">
       <div className="sidebar">
         <FriendsList />
-        <FormAddFriend />
-        <Button>Add friend</Button>
+        {showAddFriend && <FormAddFriend />}
+        <Button onClick={handlShowAddFriend}>
+          {showAddFriend ? "Close" : "Add friend "}
+        </Button>
       </div>
       <FormSplitBill />
     </div>
@@ -61,9 +77,7 @@ function Friend({ friend }) {
     </li>
   );
 }
-function Button({ children }) {
-  return <button className="button">{children}</button>;
-}
+
 function FormAddFriend() {
   return (
     <form className="form-add-friend">
